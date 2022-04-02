e6d45850cae2b32b6c4a9dcdec4b1fd35db93440
mlbarre <mlbarrera1538@gmail.com>
2022-04-01 19:00:15 -0500

    fixed minor error

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,22 +1,6 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
-  type Thought {
-    _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
-  }
-  type Reaction {
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String
-  }
   type User {
     _id: ID
     username: String
@@ -25,10 +9,28 @@ const typeDefs = gql`
     thoughts: [Thought]
     friends: [User]
   }
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
   type Auth {
     token: ID!
     user: User
   }
+
   type Query {
     me: User
     users: [User]
@@ -36,6 +38,7 @@ const typeDefs = gql`
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
+
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
@@ -45,5 +48,4 @@ const typeDefs = gql`
   }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
