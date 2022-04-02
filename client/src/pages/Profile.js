724b381b33e1c5268670b7ff69abb88eaa105af4
mlbarre <mlbarrera1538@gmail.com>
2022-04-01 19:37:24 -0500

    created reactionForm and ThoughtForm

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 
+import ThoughtForm from '../components/ThoughtForm';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
-import ThoughtForm from '../components/ThoughtForm';
-import { ADD_FRIEND } from '../utils/mutations';
+
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 import Auth from '../utils/auth';
-
-
 
 const Profile = (props) => {
   const { username: userParam } = useParams();
 
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
 
   const user = data?.me || data?.user || {};
-  const [addFriend] = useMutation(ADD_FRIEND);
 
   // redirect to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
@@ -38,10 +37,11 @@ const Profile = (props) => {
       </h4>
     );
   }
+
   const handleClick = async () => {
     try {
       await addFriend({
-        variables: { id: user._id }
+        variables: { id: user._id },
       });
     } catch (e) {
       console.error(e);
@@ -52,15 +52,15 @@ const Profile = (props) => {
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-        Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-      </h2>
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+        </h2>
 
-      {userParam && (
-        <button className="btn ml-auto" onClick={handleClick}>
-          Add Friend
-      </button>
-    )}
-    </div>
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
+      </div>
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
