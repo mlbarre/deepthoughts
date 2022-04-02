45de3e9f32cf88157b18f3dfda5ad2bd89325bed
mlbarre <mlbarrera1538@gmail.com>
2022-04-01 19:27:52 -0500

    added thoughtForm

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,10 +3,13 @@ import { Redirect, useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
-
-import { useQuery } from '@apollo/client';
+import ThoughtForm from '../components/ThoughtForm';
+import { ADD_FRIEND } from '../utils/mutations';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
+
+
 
 const Profile = (props) => {
   const { username: userParam } = useParams();
@@ -16,6 +19,7 @@ const Profile = (props) => {
   });
 
   const user = data?.me || data?.user || {};
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   // redirect to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
@@ -34,14 +38,29 @@ const Profile = (props) => {
       </h4>
     );
   }
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-        </h2>
-      </div>
+        Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+      </h2>
+
+      {userParam && (
+        <button className="btn ml-auto" onClick={handleClick}>
+          Add Friend
+      </button>
+    )}
+    </div>
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
@@ -59,6 +78,7 @@ const Profile = (props) => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
